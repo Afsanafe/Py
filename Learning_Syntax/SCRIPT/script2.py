aa8b0554d0c46c63582c00c2ec6e2f17c6e2ef30
Afsa Nafe <afsanafe1@gmail.com>
2025-09-03 14:30:10 -0400

    script1 and script2 implementation: their import logic REVERSED

--- Learning_Syntax/SCRIPT/script2.py
+++ Learning_Syntax/SCRIPT/script2.py
@@ -1,1 +1,11 @@
-print(__name__)
+from script1 import *
+
+print(__name__) # This time around reversed
+'''
+It will first run print in script1 as it was imported, which will output
+the module/file name "script1"
+
+Then after
+
+It will run print in this module/file which will print "__main__"
+'''
