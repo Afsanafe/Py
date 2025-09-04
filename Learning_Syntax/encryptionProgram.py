a034eceee0119a547c23afd090075abed3bcf4a6
Afsa Nafe <afsanafe1@gmail.com>
2025-09-04 10:34:16 -0400

    refactored code: adjusted with main and helper functions along with new logic for new and old decryption

--- Learning_Syntax/encryptionProgram.py
+++ Learning_Syntax/encryptionProgram.py
@@ -5,43 +5,82 @@ import string # We will be using some constants from the string module, rather t
 
 
 def run():
-    chars = " " + string.punctuation + string.digits + string.ascii_letters # constants: punctuation, digits, ascii_letters, 
-    # print(chars) # will print them all in order
-    chars = list(chars) # Before chars was basically a variable of strings, which is why it outputted one continguous string, BUT NOW its a list (type)
-                        #   So list(chars) turns "ab" into ['a', 'b'], and print then shows that literal-style view where the each character has single quotes, and commas between each other
-    
-    key = chars.copy() # we make a copy, SO 'chars' will display the original message, while 'key', which is a clone of 'chars', will be encrypted, and display that new encrypted messages (shuffled)
-
-    random.shuffle(key) # this mixes up the combination of elements in the list, basically every character in order, has an equivalent character in the shuffle, so if their places match, that character you see in key is what's replacing the character in our string
-
-    # Hide this to avoid seeing which characters are associated to which in the new shuffled combination
-    # print(f"chars: {chars}")
-    # print(f"key: {key}")
-
-        #ENCRYPT
-
-    plain_text = input("Enter a message to ENCRYPT: ")
-    cipher_text = ""
-
-    for letter in plain_text: # this will iterate through each character in our string we inputted to 'plain_text'
-        index = chars.index(letter) # the index(letter) method returns the position of the first match between our 'plain_text' character and 'chars' character, this position will tell which value to pass in from our 'keys' variable later
-        cipher_text += key[index]
-
-    
-    print(f"original message: {plain_text}")
-    print(f"encrypted message: {cipher_text}")
 
 
-        #DECRYPT
-    cipher_text = input("Enter a message to DECRYPT: ")
-    plain_text = ""
+    ########################################################################################################################################################################################################################
+        #DECRYPT : Helper methods (defined before invocation)
+
+    def decrypt_orig_text(cipher_text, key, chars):
+        plain_text = ""
+
+        for letter in cipher_text: # this will iterate through each character in our string we inputted to 'plain_text'
+            index = key.index(letter) # the index(letter) method returns the position of the first match between our 'plain_text' character and 'chars' character, this position will tell which value to pass in from our 'keys' variable later
+            plain_text += chars[index]
+
+        print(f"encrypted message: {cipher_text}")
+        print(f"decrypted message: {plain_text}")
 
 
-    for letter in cipher_text: # this will iterate through each character in our string we inputted to 'plain_text'
-        index = key.index(letter) # the index(letter) method returns the position of the first match between our 'plain_text' character and 'chars' character, this position will tell which value to pass in from our 'keys' variable later
-        plain_text += chars[index]
+    def decrype_new_text(key, chars):
+        cipher_text = input("Enter a message to DECRYPT: ")
+        plain_text = ""
 
+        for letter in cipher_text: # this will iterate through each character in our string we inputted to 'plain_text'
+            index = key.index(letter) # the index(letter) method returns the position of the first match between our 'plain_text' character and 'chars' character, this position will tell which value to pass in from our 'keys' variable later
+            plain_text += chars[index]
+
+        print(f"encrypted message: {cipher_text}")
+        print(f"decrypted message: {plain_text}")
+########################################################################################################################################################################################################################
     
 
-    print(f"encrypted message: {cipher_text}")
-    print(f"decrypted message: {plain_text}")
+    def main():
+        chars = " " + string.punctuation + string.digits + string.ascii_letters # constants: punctuation, digits, ascii_letters, 
+        # print(chars) # will print them all in order
+        chars = list(chars) # Before chars was basically a variable of strings, which is why it outputted one continguous string, BUT NOW its a list (type)
+                            #   So list(chars) turns "ab" into ['a', 'b'], and print then shows that literal-style view where the each character has single quotes, and commas between each other
+        
+        key = chars.copy() # we make a copy, SO 'chars' will display the original message, while 'key', which is a clone of 'chars', will be encrypted, and display that new encrypted messages (shuffled)
+
+        random.shuffle(key) # this mixes up the combination of elements in the list, basically every character in order, has an equivalent character in the shuffle, so if their places match, that character you see in key is what's replacing the character in our string
+
+        # Hide this to avoid seeing which characters are associated to which in the new shuffled combination
+        # print(f"chars: {chars}")
+        # print(f"key: {key}")
+
+            #ENCRYPT
+
+        plain_text = input("Enter a message to ENCRYPT: ")
+        cipher_text = ""
+
+        for letter in plain_text: # this will iterate through each character in our string we inputted to 'plain_text'
+            index = chars.index(letter) # the index(letter) method returns the position of the first match between our 'plain_text' character and 'chars' character, this position will tell which value to pass in from our 'keys' variable later
+            cipher_text += key[index]
+
+        
+        print(f"original message: {plain_text}")
+        print(f"encrypted message: {cipher_text}")
+
+        choice = input("Would you like to decrypt this encrypted message?(Y/N) ").upper()
+        
+
+
+        if choice == 'Y':
+            decrypt_orig_text(cipher_text, key, chars)
+
+        choice2 = input("Would you like to decrypt another message?(Y/N) ").upper()
+
+        if choice2 == 'Y':
+            decrype_new_text(key, chars)
+        
+        return 0
+    
+    main()
+    
+if __name__ == '__main__':
+    run()
+
+
+
+
+
