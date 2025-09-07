ff03b75b6413c31cd3098d4b859f9aa4cbc6ff50
Afsa Nafe <afsanafe1@gmail.com>
2025-09-07 19:44:02 -0400

    Added new --name option/flag to eve command, allowing users to replace

--- Learning_Syntax/cli/eve/cli.py
+++ Learning_Syntax/cli/eve/cli.py
@@ -1,6 +1,11 @@
 import click
 
-                # Without this decorator cli() would just be a normal python function
-@click.command() # this decorator basically says “Treat the function cli as a command-line entry point.”
-def cli():
-    click.echo('Hello, world!')
+#                 # Without this decorator cli() would just be a normal python function
+# @click.command() # this decorator basically says “Treat the function cli as a command-line entry point.”
+# def cli():
+#     click.echo('Hello, world!')
+
+@click.command()
+@click.option('--name', default='world', help='Who to greet.')
+def cli(name):
+    click.echo(f'Hello, {name}!')
