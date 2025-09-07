431214adfca55cda61006fa8cca192bdd387cd9c
Afsa Nafe <afsanafe1@gmail.com>
2025-09-07 19:35:39 -0400

    Clarified comments

--- Learning_Syntax/cli/eve/cli.py
+++ Learning_Syntax/cli/eve/cli.py
@@ -1,5 +1,6 @@
 import click
 
-@click.command() # what does this decorator do
+                # Without this decorator cli() would just be a normal python function
+@click.command() # this decorator basically says “Treat the function cli as a command-line entry point.”
 def cli():
     click.echo('Hello, world!')
